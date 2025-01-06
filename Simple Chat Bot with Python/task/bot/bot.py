3f9061a43094bca558ccc772ae03432ec58c5e07
James McDonald <jamesmcdonald112@yahoo.ie>
2025-01-06 13:15:24 +0000

    Stage 3

--- Simple Chat Bot with Python/task/bot/bot.py
+++ Simple Chat Bot with Python/task/bot/bot.py
@@ -1,15 +1,18 @@
-def greet(bot_name, birth_year):
-    print(f"Hello! My name is {bot_name}.")
-    print(f"I was created in {birth_year}.")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
 
+name = input()
 
-def remind_name():
-    print("Please, remind me your name.")
-    # reading a name
-    your_name = input()
-    print(f"What a great name you have, {your_name}!")
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
 
+# reading all remainders
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
 
-# Now we can use these functions
-greet("Aid", 2023)
-remind_name()
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
